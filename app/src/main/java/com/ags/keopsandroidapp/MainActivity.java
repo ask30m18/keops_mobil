7c52ebb9580d16ce37880e0474aa06345545a48e
AGS <ahmetgokhansener@gmail.com>
2018-11-22 16:06:36 +0300

    Album sayfası oluşturuldu, ikinci aktivite eklendi.

--- app/src/main/java/com/ags/keopsandroidapp/MainActivity.java
+++ app/src/main/java/com/ags/keopsandroidapp/MainActivity.java
@@ -1,11 +1,34 @@
 package com.ags.keopsandroidapp;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Menu olusturuldu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater .inflate(R.menu.add_albums, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Menu item'nin secilmesi kontrolu
+        if(item.getItemId() == R.id.add_albums){
+            //MainActivit2'nin calismasi saglaniyor
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
